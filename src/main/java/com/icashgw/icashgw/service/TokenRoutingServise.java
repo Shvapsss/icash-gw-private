8f395bd3f6038a8f02eee33bed9d36da69116737
Sergey Asikin <s.asikin@icash.ru>
2021-04-19 21:41:42 +0300

    mongo

--- src/main/java/com/icashgw/icashgw/service/TokenRoutingServise.java
+++ src/main/java/com/icashgw/icashgw/service/TokenRoutingServise.java
@@ -1,5 +1,6 @@
 package com.icashgw.icashgw.service;
 
+import com.icashgw.icashgw.entity.MapCashBox;
 import com.icashgw.icashgw.repo.MapCashRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,21 +14,21 @@ public class TokenRoutingServise {
     private MapCashRepository mapCashRepository;
 
     public List<String> hosts(String token){
-        return mapCashRepository.findById(token).get().getHost();
+
 //
-//        ArrayList arrayList = new ArrayList();
-//        arrayList.add("1/1");
-//        arrayList.add("1/2");
-//        arrayList.add("1/3");
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("1/1");
+        arrayList.add("1/2");
+        arrayList.add("1/3");
 
         //arrayList.add(mapCashRepository.findById(token));
-//        MapCash mapCash = new MapCash();
-//        mapCash.setToken("132t");
-//        mapCash.setHost(arrayList);
-//        mapCashRepository.save(mapCash);
+        MapCashBox mapCashBox = new MapCashBox();
+        mapCashBox.setToken("132t");
+        mapCashBox.setHost(arrayList);
+        mapCashRepository.save(mapCashBox);
 
         //UUID.randomUUID();
-
+        return mapCashRepository.findById(token).get().getHost();
 
     }
 
